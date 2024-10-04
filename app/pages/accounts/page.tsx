6138afbfb80fd5a3dba52eb83438ec2f798ffba3
roelfp <roelf.pretorius@qlink.co.za>
2024-10-04 21:11:53 +0200

    Fixed Error on accounts page.

--- app/pages/accounts/page.tsx
+++ app/pages/accounts/page.tsx
@@ -2,22 +2,22 @@ import Footer from "@/app/components/footer";
 import Navbar from "@/app/components/navbar";
 import TabBar from "@/app/components/tabBar";
 import getAuth from "@/auth";
-import { sql } from "@vercel/postgres";
-import { redirect } from "next/navigation";
+// import { sql } from "@vercel/postgres";
+// import { redirect } from "next/navigation";
 
 export default async function Accounts() {
   const auth = getAuth();
   const user = await auth.user();
   const subscription = await auth.subscription();
 
-  async function create(formData: FormData) {
-    "use server";
-    const { rows } = await sql`
-    INSERT INTO products (name)
-    VALUES (${formData.get("name")})
-  `;
-    redirect(`/product/${rows[0].slug}`);
-  }
+  //   async function create(formData: FormData) {
+  //     "use server";
+  //     const { rows } = await sql`
+  //     INSERT INTO products (name)
+  //     VALUES (${formData.get("name")})
+  //   `;
+  //     redirect(`/product/${rows[0].slug}`);
+  //   }
 
   return (
     <>
@@ -27,10 +27,10 @@ export default async function Accounts() {
       <main className="flex min-h-screen flex-col items-center justify-start p-6 lg:p-24">
         <div>Accounts</div>
 
-        <form action={create}>
+        {/* <form action={create}>
           <input type="text" name="name" />
           <button type="submit">Submit</button>
-        </form>
+        </form> */}
       </main>
 
       <Footer />
